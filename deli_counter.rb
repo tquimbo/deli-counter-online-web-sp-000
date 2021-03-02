41f891c2f3a9d776446583fba5da7df0c08f2ae7
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 22:43:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 # Write your code here.
 katz_deli = []
 
-def line
+def line(deli_counter)
 #shows place in life if empty, puts ...
 #how to show place in line? you can show in index number? find index?
-#i need to go through katz_deli
+#i need to go through katz_deli, and a person in life
+deli_counter == 0
 katz_deli = []
 puts katz_deli.count()
 if katz_deli.count() == 0
