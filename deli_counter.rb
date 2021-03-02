d0420cdff7fe12f0b91aebbc6ab6c83a9906f2f4
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 22:38:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,11 @@ def line
 #how to show place in line? you can show in index number? find index?
 #i need to go through katz_deli
 counter = 0
-if katz_deli; 
-    
+if katz_deli;
+
 elsif counter = 0
   puts "The line is currently empty."
-  
+
 end
 
 def take_a_number()
