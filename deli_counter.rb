1bfb1fd9c06101f0d506e2c2442bba98e39f8a0d
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 22:38:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
+def line
 #shows place in life if empty, puts ...
 #how to show place in line? you can show in index number? find index?
-
-    puts "The line is currently empty."
-
+#i need to go through katz_deli
+counter = 0
+if katz_deli; 
+    
+elsif counter = 0
+  puts "The line is currently empty."
+  
 end
 
 def take_a_number()
