dec8c5332617edcc4b086ad379745497c3b4c204
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 22:14:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+#shows place in life if empty, puts ...
+#how to show place in line? you can show in index number? find index?
+puts '#{katz_deli}'
+  if '#{katz_deli}' == 0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number()
+
+end
+
+def now_serving()
+
+end
