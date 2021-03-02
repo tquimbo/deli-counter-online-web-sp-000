195f74728b0315aae0007d28b625578b53db71cf
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 22:54:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,28 @@
 # Write your code here.
-katz_deli = []
-
-def line(deli_counter)
-#shows place in life if empty, puts ...
-#how to show place in line? you can show in index number? find index?
-#i need to go through katz_deli, and a person in life
-deli_counter == 0
-deli_counter.each do |customer|
-  deli_counter +=1
-end
-
-if deli_counter == 0
-  puts "The line is currently empty."
-end
-return deli_counter
-end
-
-def take_a_number(deli_counter)
-deli_counter == 0
-if deli_counter == 0
-  deli_counter += 1
-elsif deli_counter > 0
-  katz_deli.push (1)
-
-end
-
-def now_serving(deli_counter)
-deli_counter == 0
-  if deli_counter == 0
+def line(deli_array)
+  if deli_array.length == 0
     puts "The line is currently empty."
+  else
+    line_output = []
+    deli_array.each.with_index(1) do |name, index|
+      line_output << "#{index}. #{name}"
+    end
+    output = line_output.join(" ")
+    puts "The line is currently: #{output}"
+  end
+end
+
+def take_a_number(deli_array, name)
+  deli_array << name
+  place_in_line = deli_array.index(name) + 1
+  puts "Welcome, #{name}. You are number #{place_in_line} in line."
+end
+
+def now_serving(deli_array)
+  if deli_array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_array[0]}."
+    deli_array.shift
+  end
 end
